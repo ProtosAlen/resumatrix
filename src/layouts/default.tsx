0e9a4cf38b6fb214754c76c41b95befd9476b653
Alenn <alenn@infofix.eu>
2025-06-11 13:11:44 +0200

    Copy

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -12,7 +12,7 @@ export default function DefaultLayout({
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3">
-
+        Made by Alen Naumoski - 2025
       </footer>
     </div>
   );
